1e329f1d71793d363f0a498bee62e203363190f1
Paul Wamaria <paulwamaria@gmail.com>
2020-09-21 12:02:58 +0300

    Render couresel as a child of Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Couresel from  "./Couresel"
 
 export class AboutUs extends Component {
     render() {
         return (
             <div>
-                Projects I have worked on
+               <h3 className="funnyFonts text-white text-center mt-2 mb-3">Projects I have worked on:</h3>
+                <Couresel />
             </div>
         )
     }
