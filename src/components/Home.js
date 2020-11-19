d461820532866e308457000319a10be6e7f69be0
Paulwamaria <paulwamaria@gmail.com>
2020-11-19 15:30:10 +0300

    Change about me

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,9 +20,10 @@ export class Home extends Component {
               </div>
               <h4 className="text-center text-white funnyFonts mb-2">
                 Paul Wamaria
-              </h4 >
-              <h3 className="text-center text-white funnyFonts mb-2">A Passionate Full Stack Developer</h3>
-
+              </h4>
+              <h3 className="text-center text-white funnyFonts mb-2">
+                A Passionate Full Stack Developer
+              </h3>
             </div>
             <div className="col-md-6 #9699bb">
               <div className=" text-white hoverLight overflow-auto mt-3">
@@ -34,18 +35,17 @@ export class Home extends Component {
                     I am a passionate full-stack developer with experience in
                     the software development life cycle. From design,
                     development, to production, I have familiarized myself with
-                    tools such as Photoshop, Illustrator, Python, Javascript,
-                    Django, React, Flask, Angular, React-redux among others. I
-                    am also comfortable creating and consuming APIs. Recently, I
-                    have developed a keen interest in cloud services. Exercising
-                    is my way of decompressing. If I am not coding, or by any
+                    technologies such as Python, Javascript, Django, React,
+                    Flask, Angular, React-redux among others. I am also
+                    comfortable creating and consuming APIs. Recently, I have
+                    developed a keen interest in cloud services. Exercising is
+                    my way of decompressing. If I am not coding, or by any
                     chance, if I need to get away from the bugs for a while,
-                    you’ll either find me in the dojo kicking and punching or on
-                    a yoga mat stretching. I consider having a growth mindset as
-                    my greatest asset because it compels me to seek a
-                    challenging and dynamic environment where I can grow.
+                    you’ll either find me on a yoga mat stretching. I consider
+                    having a growth mindset as my greatest asset because it
+                    compels me to seek a challenging and dynamic environment
+                    where I can grow.
                   </p>
-                  
                 </div>
               </div>
             </div>
